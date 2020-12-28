3bb5a33e2b4356cc01a6756d04c9763613b58af6
A. Nikhil Teja <62937562+nikhiltejaa@users.noreply.github.com>
2020-12-28 18:43:15 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,48 @@
 #include <iostream>
 using namespace std;
 
-int main()
+bool checkPrime(int n);
+
+int main() {
+    int n, i;
+    bool flag = false;
+
+    cout << "Enter a positive  integer: ";
+    cin >> n;
+
+    for(i = 2; i <= n/2; ++i) {
+        if (checkPrime(i)) {
+            if (checkPrime(n - i)) {
+                cout << n << " = " << i << " + " << n-i << endl;
+                flag = true;
+            }
+        }
+    }
+
+    if (!flag)
+      cout << n << " can't be expressed as sum of two prime numbers.";
+
+    return 0;
+}
+
+// Check prime number
+bool checkPrime(int n)
 {
-   int data[5];
-   cout << "Enter elements: ";
+    int i;
+    bool isPrime = true;
 
-   for(int i = 0; i < 5; ++i)
-      cin >> data[i];
+    // 0 and 1 are not prime numbers
+    if (n == 0 || n == 1) {
+        isPrime = false;
+    }
+    else {
+        for(i = 2; i <= n/2; ++i) {
+            if(n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
 
-   cout << "You entered: ";
-   for(int i = 0; i < 5; ++i)
-      cout << endl << *(data + i);
-
-   return 0;
+    return isPrime;
 }
